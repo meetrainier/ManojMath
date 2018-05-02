422a59a386dc01d06f91ed436bbb989516ac7052
Manoj <manojcad@yahoo.com>
2018-05-02 10:47:10 -0700

    Update hackeralgo1.cpp

--- hackeralgo1.cpp
+++ hackeralgo1.cpp
@@ -3,10 +3,59 @@
 //ezfnjymgqtjnmstbadgdsrxvntnacwljnkgchtjeaoivfcindgxipmrjuqmmcpntpotplodjhijxqpogjmzipygacfdjgmewechuebxvcbnakszzcxkozxwavzgmesrvysonomhvufezislfntgncspthcpneyminpbjildobozfirvcgdratdpmmpkujcywvtzkdytzyfejbytsobvudvutfueveevgrqnxjiwpkrvllsjxmqhotlnpgjxkjnobxfqodlyiqsisdeuwqmntxouzdtisgutdafostmwticvncjwldpknuodmfksusaqpsoosgpiveyxipfklmhypdxpdncpgaswpycoxsuxasqduojpblctcyvyxldcgzevedvxiwinfppkjbtifuuapickknwxxjmjmtxlpfalxdgepmekaxijuphqfafrnezyldokwcnzenhpibktlfuxjfmeqajmvopbhuslnnnlmkmoteceiwbytjhhxqnkuazevswrkaofggfrnapciuoexqogscugzspwuvzkyrdfkhixcaqctfwadewpqksxxvqiigvjjpagvqikuojlwhfyztu
 //expected output 
 //0
-
+//Test2
+//180
+//czoczkotespkfjnkbgpfnmtgqhorrzdppcebyybhlcsplqcqogqaszjgorlsrppinhgpaweydclepyftywafupqsjrbkqakpygolyyfksvqetrfzrcmatlicxtcxulwgvlnslazpfpoqrgssfcrfvwbtxaagjfahcgxbjlltfpprpcjyivxu
+//6
+//
+//Test 3
+//187
+//tlymrvjcylhqifsqtyyzfaugtibkkghfhyzxqbsizkjguqlqwwetyofqihtpkmpdlgthfybfhhmjerjdkybwppwrdapirukcshkpngayrdruanjtziknnwxmsjpnuswllymhkmztsrcwwzmlbcoakswwffveobbvzinkhnmvwqhpfednhsuzmffaebi
+//0
 #include <bits/stdc++.h>
 
 using namespace std;
+
+int create_alt_str(char a, char b, string s) 
+{
+    string s1(s);
+    //deletable d(a,b);
+    //remove_if(begin(s1),end(s1),d());
+    s1.erase(std::remove_if(s1.begin(), 
+                              s1.end(),
+                              [a,b](unsigned char x){return (x!=a && x!=b);}),
+               s1.end());
+    bool bad=false;
+    if(s1.length() > 1)
+    {
+        if(s1[0]!=s1[1])
+        {
+            for (int i = 0; i< s1.size() ; ++i) 
+            {
+                if(i%2==0 && s1[i]!=s1[0])
+                {
+                    bad = true;
+                    break;
+                }
+                if(i%2==1 && s1[i]!=s1[1])
+                {
+                    bad = true;
+                    break;
+                }
+            }
+        }
+        else
+        {
+            bad = true;
+        }
+    }
+    if(bad)
+        return 0;
+    cerr << a << " " << b << " " << s1 << " " << s1.size() << endl; 
+    return s1.size();
+}
+
+/*
 int create_alt_str(char a, char b, string s) 
 {
     vector<char> vs;
@@ -29,7 +78,7 @@ int create_alt_str(char a, char b, string s)
     cerr << a << " " << b << " " << tmp << " " << vs.size() << endl; 
     return vs.size();
 }
-
+*/
 int get_max_alt_str(char a, char b, string s) 
 {
     auto m1 = create_alt_str(a,b,s);
